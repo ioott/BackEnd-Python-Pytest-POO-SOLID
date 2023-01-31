67328a5f9508511f5cdae8f5a3b57c5947bef87b
ioott <vioott@gmail.com>
2023-01-31 16:40:43 -0300

    test: cria o teste da classe product Este teste certifica que o construtor da classe está funcionando corretamente, verificando o correto preenchimento dos atributos.

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,17 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = "arroz"
+    empresa = "Maricota"
+    instrucoes = "ao abrigo do sol"
+
+    product_instance = Product(1, produto, empresa, 2022, 2023, 12, instrucoes)
+
+    assert product_instance.id == 1
+    assert product_instance.nome_do_produto == "arroz"
+    assert product_instance.nome_da_empresa == "Maricota"
+    assert product_instance.data_de_fabricacao == "2022"
+    assert product_instance.data_de_validade == "2023"
+    assert product_instance.numero_de_serie == 12
+    assert product_instance.instrucoes_de_armazenamento == "ao abrigo do sol"
