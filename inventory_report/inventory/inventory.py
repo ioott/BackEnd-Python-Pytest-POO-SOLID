1c2864719798f6719c99254ba67fe438f3945cdc
ioott <vioott@gmail.com>
2023-02-01 20:21:09 -0300

    feat: adiciona a funcionalidade de ler arquivos json à classe Inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -8,11 +9,15 @@ class Inventory:
     @staticmethod
     def import_data(path: str, report_type: str):
 
-        prod_data = []
-        with open(path, "r") as file:
-            all_products = csv.DictReader(file)
-            for product in all_products:
-                prod_data.append(dict(product))
+        if path.endswith(".csv"):
+            prod_data = []
+            with open(path, "r") as file:
+                all_products = csv.DictReader(file)
+                for product in all_products:
+                    prod_data.append(dict(product))
+        elif path.endswith(".json"):
+            with open(path, mode="r") as file:
+                prod_data = json.load(file)
 
         if report_type == "simples":
             report = SimpleReport.generate(prod_data)
