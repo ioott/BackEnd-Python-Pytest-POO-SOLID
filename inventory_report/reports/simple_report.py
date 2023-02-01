e1665bff34d4c00de4bea58a3ba0f58a1c98d9c0
ioott <vioott@gmail.com>
2023-02-01 12:11:32 -0300

    feat: cria a versão simplificada do relatório em simple_report.py Este relatório informa qual a data de fabricação mais antiga, qual a data de validade está mais próxima, dentre os produtos que ainda não venceram e qual empresa tem mais produtos.

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,22 @@
+from datetime import date
+
+
 class SimpleReport:
-    pass
+
+    @staticmethod
+    def generate(prod_data: list[dict]):
+        old_fact = min(product["data_de_fabricacao"] for product in prod_data)
+        exp_dates = min(
+            exp_date for product in prod_data
+            for exp_date in [product["data_de_validade"]]
+            if exp_date >= date.today().isoformat()
+        )
+
+        company = [product["nome_da_empresa"] for product in prod_data]
+        company = max(set(company), key=company.count)
+
+        return (
+            f'Data de fabricação mais antiga: {old_fact}\n'
+            f'Data de validade mais próxima: {exp_dates}\n'
+            f'Empresa com mais produtos: {company}'
+        )
