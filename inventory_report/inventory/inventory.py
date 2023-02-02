9340c5668095aff48f258e394dc3ea8eee4cc405
ioott <vioott@gmail.com>
2023-02-02 20:12:56 -0300

    refat: diminui a complexidade de inventory.py

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -9,28 +9,39 @@ class Inventory:
 
     @staticmethod
     def import_data(path: str, report_type: str):
-
-        prod_data = []
-
         if path.endswith(".csv"):
-            with open(path, "r") as file:
-                all_products = csv.DictReader(file)
-                for product in all_products:
-                    prod_data.append(dict(product))
-
+            prod_data = Inventory.csv(path)
         elif path.endswith(".json"):
-            with open(path, mode="r") as file:
-                prod_data = json.load(file)
-
+            prod_data = Inventory.json(path)
         elif path.endswith(".xml"):
-            with open(path, mode="r") as file:
-                file = file.read()
-                file = xmltodict.parse(file)
-                file = file['dataset']['record']
+            prod_data = Inventory.xml(path)
+        return Inventory.generate_report(prod_data, report_type)
 
-                for line in file:
-                    prod_data.append(dict(line))
+    def csv(path):
+        prod_data = []
+        with open(path, "r") as file:
+            all_products = csv.DictReader(file)
+            for product in all_products:
+                prod_data.append(dict(product))
+        return prod_data
 
+    def json(path):
+        with open(path, mode="r") as file:
+            prod_data = json.load(file)
+            return prod_data
+
+    def xml(path):
+        prod_data = []
+        with open(path, mode="r") as file:
+            file = file.read()
+            file = xmltodict.parse(file)
+            file = file['dataset']['record']
+
+            for line in file:
+                prod_data.append(dict(line))
+        return prod_data
+
+    def generate_report(prod_data, report_type):
         if report_type == "simples":
             report = SimpleReport.generate(prod_data)
             return report
