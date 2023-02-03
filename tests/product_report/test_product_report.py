41d2d052135042713615cc544d80d9330d76bba6
ioott <vioott@gmail.com>
2023-02-02 21:47:14 -0300

    test: testa a função Product em product.py Deve instanciar um objeto Product e verificar se, ao acessá-lo, a frase de retorno esta correta.

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_prod = Product(
+        id=1,
+        nome_do_produto="massa de tomate",
+        nome_da_empresa="trybe alimentos ltda",
+        data_de_fabricacao="2022-09-09",
+        data_de_validade="2023-04-09",
+        numero_de_serie="333b33",
+        instrucoes_de_armazenamento="Guarde bem",
+    )
+    expect = (
+            "O produto massa de tomate"
+            " fabricado em 2022-09-09"
+            " por trybe alimentos ltda com validade"
+            " até 2023-04-09"
+            " precisa ser armazenado Guarde bem."
+        )
+    assert str(new_prod) == expect
